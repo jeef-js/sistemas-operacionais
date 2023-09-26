dc223d3c908bc76de97fdd487a816ee232c316c0
Jefferson Thiago Gomes de Araújo <v001250@intrajus.tjrn@dev-dps-11.intrajus.tjrn>
2023-09-26 15:50:31 -0300

    updating main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
+#include <tuple>
 #include "../include/Matrix.h"
+
+using namespace std;
+
+Matrix *buildMatrix();
 
 int main(int argc, char const *argv[])
 {
-  Matrix *matrix = new Matrix(3, 3);
-  matrix->randomFill();
+  Matrix *matrix = buildMatrix();
 
-  std::cout << matrix->toString();
+  cout << matrix->toString();
 
   return 0;
+}
+
+Matrix *buildMatrix()
+{
+  tuple<int, int> matrixDimensions = make_tuple(0, 0);
+  int input;
+
+  cout << "Informe a quantidade de linhas: " << endl
+       << ">> ";
+  cin >> input;
+
+  get<0>(matrixDimensions) = input;
+
+  cout << "Informe a quantidade de colunas: " << endl
+       << ">> ";
+  cin >> input;
+
+  get<1>(matrixDimensions) = input;
+
+  Matrix *matrix = new Matrix(get<0>(matrixDimensions), get<1>(matrixDimensions));
+  matrix->randomFill();
+
+  return matrix;
 }
