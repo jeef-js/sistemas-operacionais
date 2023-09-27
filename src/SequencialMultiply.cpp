11fb3531c4667268b4b4d4a584dc5c549312e979
jeef-js <jeffersonthiago.ga@gmail.com>
2023-09-27 00:08:12 -0300

    finalizando a multiplicaco sequencial com escrita de arquivo

--- src/SequencialMultiply.cpp
+++ src/SequencialMultiply.cpp
@@ -1,8 +1,15 @@
+#include <fstream>
+#include <chrono>
 #include "../include/Matrix.h"
 
 Matrix *Matrix::operator*(const Matrix &other)
 {
+  std::fstream outputFile;
+  outputFile.open("data/resultado_multiplicacao_sequencial.txt", std::ios::out | std::ios::binary);
+  outputFile << this->rows << " " << other.columns << std::endl;
+
   Matrix *result = new Matrix(this->rows, other.columns);
+  auto startTime = std::chrono::high_resolution_clock::now();
 
   for (int i = 0; i < result->rows; i++)
   {
@@ -13,8 +20,16 @@ Matrix *Matrix::operator*(const Matrix &other)
       {
         result->data[i][j] += this->data[i][k] * other.data[k][j];
       }
+
+      outputFile << "c" << i + 1 << j + 1 << " " << result->data[i][j] << std::endl;
     }
   }
+
+  auto endTime = std::chrono::high_resolution_clock::now();
+  auto duration = endTime - startTime;
+
+  outputFile << std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() << "ms";
+  outputFile.close();
 
   return result;
 }
